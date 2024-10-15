e6defff94aceec182d7eb44287fada73f0e58530
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-15 16:59:53 +0000

    Implemented the SignInUp form by adding new components and CSS styles, configured routing, integrated axios for API requests, and made layout adjustments to fit the remeskin design vision.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import NavBar from "./components/NavBar";
 import styles from "./App.module.css";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <h1>Sign up</h1>} />
-          <Route render={() => <p>Page not found!</p>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
+          <Route render={() => <h4>Page not found!</h4>} />
         </Switch>
       </Container>
     </div>
