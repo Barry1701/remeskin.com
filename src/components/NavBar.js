01da822eda62d956d3b119e51b876332a2f33611
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-21 07:26:41 +0000

    add hooks folder and click otside toggle function

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,10 +16,13 @@ import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContex
 import Avatar from "./Avatar";
 import axios from "axios";
 import styles from "../styles/NavBar.module.css";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -69,14 +72,14 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar bg="light" expand="lg" fixed="top" className={styles.NavBar}>
+    <Navbar bg="light" expand="lg" fixed="top" className={styles.NavBar} expanded={expanded}>
       <NavLink to="/">
         <Navbar.Brand>
           <img src={logo} alt="logo" height="70" className={styles.Logo} />
         </Navbar.Brand>
       </NavLink>
       {currentUser && addPostIcon}
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Toggle ref={ref} onClick={() => setExpanded(!expanded)} aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto text-right">
           <NavLink exact to="/" className={styles.NavLink}>
