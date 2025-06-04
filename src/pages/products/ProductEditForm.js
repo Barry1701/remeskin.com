55aca9df2d652b25dfc5fa8987922c78603948fc
Barry1701 <138708567+Barry1701@users.noreply.github.com>
2025-06-04 07:18:22 +0100

    Fix react-router imports

--- src/pages/products/ProductEditForm.js
+++ src/pages/products/ProductEditForm.js
@@ -3,7 +3,7 @@ import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 import Image from "react-bootstrap/Image";
-import { useHistory, useParams } from "react-router";
+import { useHistory, useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import axios from "axios";
 import Swal from "sweetalert2";
